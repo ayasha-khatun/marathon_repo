3357fb0cd4e40b9a1736eeb89fca7625fbfc417e
ayasha-khatun <ayashakhatun004@gmail.com>
2025-06-25 20:48:41 +0600

    added dynamic title

--- marathon-management-system/src/Layouts/RootLayouts.jsx
+++ marathon-management-system/src/Layouts/RootLayouts.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../Pages/Shared/Navbar';
 import Footer from '../Pages/Shared/Footer';
+import DynamicTitle from '../Pages/Shared/DynamicTitle';
 
 const RootLayouts = () => {
     return (
         <div>
+            <DynamicTitle></DynamicTitle>
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
---
+++ marathon-management-system/src/Pages/Shared/DynamicTitle.jsx
@@ -0,0 +1,28 @@
+// src/Components/DynamicTitle.jsx
+import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+
+const DynamicTitle = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const path = location.pathname;
+
+    let title = 'MarathonPro'; 
+
+    if (path === '/') title = 'Home | MarathonPro';
+    else if (path === '/login') title = 'Login | MarathonPro';
+    else if (path === '/register') title = 'Register | MarathonPro';
+    else if (path === '/marathons') title = 'Marathons | MarathonPro';
+    else if (path.includes('/dashboard')) title = 'Dashboard | MarathonPro';
+    else if (path.includes('/marathon/')) title = 'Marathon Details | MarathonPro';
+    else if (path.includes('/register-marathon/')) title = 'Register Marathon | MarathonPro';
+    else title = 'Page Not Found | MarathonPro';
+
+    document.title = title;
+  }, [location]);
+
+  return null;
+};
+
+export default DynamicTitle;
